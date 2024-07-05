7fa950126d2e47c4f45520119b6e16e883ba68cc
BMokgola <boitumelomokgola1@gmail.com>
2024-07-05 09:59:29 +0200

    Initial commit

---
+++ counter.js
@@ -0,0 +1,21 @@
+function Counter() {
+    return {
+        count: 0,
+        max: 10,
+        increment() {
+            if (this.count < this.max) {
+                this.count++;
+            }
+        },
+        decrement() {
+            if (this.count > 0) {
+                this.count--;
+            }
+        },
+        reset() {
+            this.count = 0;
+        },
+    };
+}
+
+
